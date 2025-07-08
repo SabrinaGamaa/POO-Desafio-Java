59542722f76fb2672e25de86880593d1a3938193
SabrinaGamaa <sabrina1985janaina@gmail.com>
2025-07-08 10:33:47 -0300

    Finalizado a classe Main para o desafio

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,21 @@ public class Main {
         System.out.println(" ==== REPRODUTOR MUSICAL ===== ");
         iphone.selecionarMusica("Let it be dos Beatles");
         iphone.tocar();
+        iphone.pausar();
+        System.out.println();
+
+
+        System.out.println(" ==== LIGAÇÃO ===== ");
+        iphone.ligar("00 12344-5678");
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        System.out.println();
+
+
+        System.out.println(" ==== INTERNET ===== ");
+        iphone.exibirPagina("https://www.google.com");
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+
     }
 }
